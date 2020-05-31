949813bd331fc899d193e0da63d6f8301df4f6c3
Krzysztof PIaskowy <krzychu2956@gmail.com>
2020-05-31 12:47:42 +0200

    Structure refactor

--- include/communication/common/Internet.h
+++ include/communication/common/Internet.h
@@ -1,15 +1,27 @@
 #pragma once
 
 #include <Arduino.h>
-#include "Http.h"
+#include "communication/wifi/WiFiConn.h"
+#include "communication/gsm/GsmConn.h"
+#include "communication/common/Http.h"
 
-class GsmConn
+class Internet
 {
 
 public:
+    enum Type
+    {
+        WIFI = 0,
+        GSM = 1
+    };
+
+    static void setType(Type type);
     static int start();
     static void stop();
     static Http::Response httpGet(String url);
     static Http::Response httpPost(String url, String json);
     static Http::Response httpPut(String url, String json);
+
+private:
+    static Type type;
 };
