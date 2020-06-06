651c063dca83b75ab08711455e66f3a1d7221d96
jakubdybczak <jakub.dybczak@gmail.com>
2020-06-07 01:16:28 +0200

    Add publishing example location and address

--- src/mock/main.cpp
+++ src/mock/main.cpp
@@ -1,26 +1,23 @@
-#include <Arduino.h>
-#include <ArduinoJson.h>
-#include "config/Config.h"
+#include "api/Api.h"
 #include "communication/bluetooth/Bluetooth.h"
 #include "communication/bluetooth/BluetoothHandler.h"
 #include "communication/common/Internet.h"
+#include "config/Config.h"
 #include "maintenance/Logger.h"
-#include "api/Api.h"
+#include <Arduino.h>
+#include <ArduinoJson.h>
 
 bool refreshRequested = false;
 unsigned long lastPublishMillis = 0;
 
-class MyBluetoothHandler : public BluetoothHandler
-{
-  void deviceRefreshRequest()
-  {
+class MyBluetoothHandler : public BluetoothHandler {
+  void deviceRefreshRequest() {
     Serial.println("Refresh request!");
     refreshRequested = true;
   }
 };
 
-void setup()
-{
+void setup() {
   Serial.begin(115200);
   Config::instance().load();
   Bluetooth::start(new MyBluetoothHandler());
@@ -28,27 +25,27 @@ void setup()
   Internet::start();
 }
 
-void loop()
-{
-  if (refreshRequested)
-  {
+void loop() {
+  if (refreshRequested) {
     Config::instance().save();
     Api.configUpdated();
     refreshRequested = false;
   }
-  
-  if ((millis() - lastPublishMillis) > 10000)
-  {
+
+  if ((millis() - lastPublishMillis) > 10000) {
     Logger::debug("Publishing data");
     double temperature = random(15 * 100, 25 * 100) / 100;
     double humidity = random(0 * 100, 100 * 100) / 100;
+    double pm1 = random(20 * 100, 40 * 100) / 100;
+    double pm2_5 = random(10 * 100, 20 * 100) / 100;
     double pm10 = random(20 * 100, 40 * 100) / 100;
-    double pm25 = random(10 * 100, 20 * 100) / 100;
 
     Api.publishMeasurement("temperature", temperature);
     Api.publishMeasurement("humidity", humidity);
+    Api.publishMeasurement("pm1", pm1);
+    Api.publishMeasurement("pm2_5", pm2_5);
     Api.publishMeasurement("pm10", pm10);
-    Api.publishMeasurement("pm25", pm25);
+
     lastPublishMillis = millis();
   }
   delay(100);
