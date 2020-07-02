8027d5ab61b8f919ae7086f65bda9aeac591ace6
Krzysztof PIaskowy <krzychu2956@gmail.com>
2020-07-02 21:44:21 +0200

    Add storage SD

--- include/device/Storage.h
+++ include/device/Storage.h
@@ -3,17 +3,19 @@
 #include "device/Sensor.h"
 #include "SD.h"
 
-struct StorageConfig {
-    const unsigned char pin = 0;
-};
-
-class Storage : public Sensor{
+class Storage : public Sensor {
 
 public:
     Storage();
     ~Storage();
+    FS *getStorage();
+    bool write(const char *message, const char *path, bool logging = true) const;
+    bool append(const char *message, const char *path, bool logging = true) const;
+    String read(const char *message, const char *path, bool logging = true) const;
+    bool remove(const char *path) const;
 
 private:
     StorageConfig config;
+    bool write(const char *message, const char *path, const char *mode, bool logging = true) const;
 
 };
