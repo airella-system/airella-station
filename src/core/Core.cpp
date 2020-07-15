670044d5b6a3f0b87dbc4bf5720705c43c26a81d
Krzysztof PIaskowy <krzychu2956@gmail.com>
2020-07-15 20:18:14 +0200

    Interactive mode

--- src/core/Core.cpp
+++ src/core/Core.cpp
@@ -19,16 +19,21 @@ Core::Core() {
 #define U2TX 17
 
 #define nG510_PWR 33
+#define SELGPS 12
 
 HardwareSerial HWSerial0(0);
 HardwareSerial HWSerial2(2);
 
 char incomingByte;
 char incomingByte2 = 0;
+int ignore = 0;
 
 void Core::setUp() {
     pinMode(nG510_PWR,OUTPUT);
     digitalWrite(nG510_PWR,HIGH);
+
+    pinMode(SELGPS,OUTPUT);
+    digitalWrite(SELGPS,LOW);
 
     HWSerial0.begin(115200, SERIAL_8N1, U0RX, U0TX, false, 1000);
     HWSerial2.begin(115200, SERIAL_8N1, U2RX, U2TX, false, 1000);
@@ -43,11 +48,27 @@ void Core::loop() {
             HWSerial0.print("\n");
             command += "\r\n";
             HWSerial2.printf(command.c_str());
+            ignore = command.length();
             command = "";
         } 
         else if(incomingByte == '*') {
             HWSerial0.print("send AT\n");
             HWSerial2.printf("at\r\n");
+            ignore = 2;
+        }
+        else if(incomingByte == '(') {
+            HWSerial0.printf("G510 Power ON signal...");
+            digitalWrite(nG510_PWR,LOW);
+            delay(900); //min 800ms
+            digitalWrite(nG510_PWR,HIGH);
+            HWSerial0.printf("done\n");
+        }
+        else if(incomingByte == ')') {
+            HWSerial0.printf("G510 Power OFF signal...");
+            digitalWrite(nG510_PWR,LOW);
+            delay(3500); //min 3s
+            digitalWrite(nG510_PWR,HIGH);
+            HWSerial0.printf("done\n");
         }
         else {
             HWSerial0.print(incomingByte);
@@ -56,12 +77,16 @@ void Core::loop() {
     }
 
     if (HWSerial2.available() > 0) {
-        HWSerial0.printf("Data from HWSerial2: ");
-        while(HWSerial2.available() > 0) {
-            incomingByte2 = HWSerial2.read();
-            HWSerial0.print(incomingByte2);
+        if(ignore) {
+            ignore--;
         }
-        HWSerial0.print("\n");
-        HWSerial0.printf("\nend\n");
+        else {
+            HWSerial0.printf("Data from HWSerial2: ");
+            while(HWSerial2.available() > 0) {
+                incomingByte2 = HWSerial2.read();
+                HWSerial0.print(incomingByte2);
+            }
+            HWSerial0.printf("------\n");
+        }
     }
 }
